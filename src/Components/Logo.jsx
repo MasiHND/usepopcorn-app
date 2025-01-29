0fdaa1189684e99c90278226b07f4db6ce687fcf
MasiHND <pouya.hendy@gmail.com>
2025-01-29 07:48:38 +0330

    adding project

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,10 @@
+import App from "../App";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn </h1>
+    </div>
+  );
+}
