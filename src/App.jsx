cd7cb486b7f44c5ff504f4c2adeee495f8c77190
MasiHND <pouya.hendy@gmail.com>
2025-02-13 21:10:34 +0330

    adding signature

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ErrorMessage from "./Components/ErrorMessage";
 import SelectedMovie from "./Components/selectedMovie";
 import { useMovies } from "./Hooks/useMovies";
 import { useLocalStorageState } from "./Hooks/useLocalStorageState";
+import AppFooter from "./Components/AppFooter";
 
 const MYKEY = "e3d6a10c";
 
@@ -75,84 +76,8 @@ export default function App() {
             </>
           )}
         </Box>
+        <AppFooter/>
       </MainLayout>
-
-      {/* <StarRating
-        maxRating={10}
-        defaultRating={6}
-        message={[
-          "Awful",
-          "Terrible",
-          "Bad",
-          "Not Good",
-          "Not Bad",
-          "Okay",
-          "Good",
-          "Very Good",
-          "Amazing",
-          "Perfect",
-        ]}
-      />
-      <Test /> */}
     </>
   );
 }
-
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-
-//   return (
-//     <div>
-//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-//       <p>This Movie Was Rated {movieRating} Stars</p>
-//     </div>
-//   );
-// }
-
-//----------------------------------------------------------
-// const tempMovieData = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt0133093",
-//     Title: "The Matrix",
-//     Year: "1999",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt6751668",
-//     Title: "Parasite",
-//     Year: "2019",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-//   },
-// ];
-
-// const tempWatchedData = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//     runtime: 148,
-//     imdbRating: 8.8,
-//     userRating: 10,
-//   },
-//   {
-//     imdbID: "tt0088763",
-//     Title: "Back to the Future",
-//     Year: "1985",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-//     runtime: 116,
-//     imdbRating: 8.5,
-//     userRating: 9,
-//   },
-// ];
